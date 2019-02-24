a3cb0c6137b1792f42cbac8311b2cb9847b12022
USUARIO <USUARIO@USUARIO-PC>
2019-02-24 17:52:37 +0100

    1º Desconexión -> Smarty, aparte del login, listar productos de la BD, añadir BTN para louout, en caso de acceder a productos.php sin loguerse notificar que tiene que loguearse...

---
+++ POO/Productos.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Clase Producto.php -> Interactuar con un producto concreto.
+ */
+
+class Productos {
+    
+    private $cod;
+    private $nombre;
+    private $name_short;
+    private $descripcion;
+    private $precio;
+    private $familia;
+    
+    /**
+     * Contructor de la clase donde intanciamos los atributos...
+     */
+    /*function __construct() {
+        $this->cod = $cod;
+        $this->nombre = $nombre;
+        $this->name_short = $name_short;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->familia = $familia;
+    }*/
+    
+}
